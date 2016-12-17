97cf9cf3e31805c60d6d7e942cbebef7dae9c922
Trillbee <adelaide@MacBook-Air.local>
2016-12-17 15:41:17 +1100

    Added login and logout functionality.

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
-    author = models.Charfield(max_length=200)
+    author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
